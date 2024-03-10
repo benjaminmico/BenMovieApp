9ca5c9ce2947f879179191f523cb45c420607b7a
Benjamin <benjamin.mico@pyypl.com>
2024-03-10 21:34:52 +0400

    chore: enrich tsconfig.json + babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(false);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+          verbose: false,
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+        },
+      ],
+    ],
+  };
 };
